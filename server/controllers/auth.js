aaf48f40a546aeee7c8e94260995677a12262c97
Arghya Maity <maityarghya0@gmail.com>
2023-09-06 01:35:32 +0530

    JWT added

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,9 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+var jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const jwtKey = process.env.JWT_SECRET;
 
 const signupUser = async (req, res) => {
   const { name, email, password } = req.body;
@@ -11,10 +15,15 @@ const signupUser = async (req, res) => {
       email,
       password: hashPassword,
     });
-    res.status(200).json(user);
-    
+    let data = {
+      email,
+      id: user._id,
+    };
+    const token = jwt.sign(data, jwtKey);
+    res.status(200).send({name: user.name, token});
+
   } catch (error) {
-    res.status(400).json(error);
+    res.status(400).send(error);
   }
 };
 
@@ -24,11 +33,15 @@ const loginUser = async (req, res) => {
     const user = await User.findOne({ email });
     const isCorrectPass = bcrypt.compareSync(password, user.password);
     if (isCorrectPass) {
-      res.status(200).json(user);
+      let data = {
+        email,
+        id: _id,
+      };
+      const token = jwt.sign(data, jwtKey);
+      res.status(200).json({name: user.name, token});
     } else {
       res.status(400).json("Incorrect credentials");
     }
-
   } catch (error) {
     res.status(400).json(error);
   }
