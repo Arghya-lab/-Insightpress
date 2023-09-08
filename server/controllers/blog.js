b3df58634e7de051bd42bb7ad38b4a06969a8084
Arghya Maity <maityarghya0@gmail.com>
2023-09-08 20:49:21 +0530

    added update, delete, authors specific blog sending api in backend.

--- server/controllers/blog.js
+++ server/controllers/blog.js
@@ -23,7 +23,7 @@ const uploadBlog = async (req, res) => {
 // Read
 const getSingleBlog = async(req, res) => {
   try {
-    const { id } = req.params
+    const { id } = req.params // blog id
     const blogData = await Blog.findById(id)
     res.status(200).json(blogData)
   } catch (error) {
@@ -41,5 +41,52 @@ const getBlogs = async(req, res) => {
   }
 }
 
+const getAuthorBlogs = async (req, res) => {
+  try {
+    const { id } = req.params  // author id || user id
+    const blogs = await Blog.find({userId: id})
+    res.status(200).send(blogs)
+  } catch (error) {
+    res.status(400).json(error)
+  }
+}
 
-module.exports = { uploadBlog, getSingleBlog, getBlogs }
+const updateBlog = async (req, res) => {
+  try {
+    const { id } = req.params // blog id
+    const { userId } = await Blog.findById(id)
+    const { title, summary, content } = req.body
+    // req.user data came via token
+    if (req.user.id) {
+      res.status(400).json({"error": "Blog not found."})
+    } else if(userId != req.user.id) {
+      res.status(400).json({"error": "You are unauthorize to delete."})
+    } else {
+      const blog = await Blog.findByIdAndUpdate(id, { title, summary, content })
+      res.status(200).json(blog)
+    }
+  } catch (error) {
+    res.status(400).json(error)
+  }
+}
+
+const deleteBlog = async (req, res) => {
+  try {
+    const { id } = req.params // blog id
+    const { userId } = await Blog.findById(id)
+    // req.user data came via token
+    if (req.user.id) {
+      res.status(400).json({"error": "Blog not found."})
+    } else if(userId != req.user.id) {
+      res.status(400).json({"error": "You are unauthorize to delete."})
+    } else {
+      await Blog.findByIdAndDelete(id)
+      res.status(200).json({ "Success": "Note has been deleted."})
+    }
+  } catch (error) {
+    res.status(400).json(error)
+  }
+}
+
+
+module.exports = { uploadBlog, getSingleBlog, getBlogs, getAuthorBlogs, updateBlog, deleteBlog }
--- server/routes/blog.js
+++ server/routes/blog.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const { uploadBlog, getSingleBlog ,getBlogs } = require('../controllers/blog')
 const fetchUser = require('../middleware/fetchUser')
+const { uploadBlog, getSingleBlog ,getBlogs, getAuthorBlogs, updateBlog, deleteBlog } = require('../controllers/blog')
 
 const router = express.Router()
 
-// posting a new blog using : POST /api/blog  : token require
+// posting a new blog using : POST /api/blog  => token require
 router.post("/", fetchUser, uploadBlog)
 
 // get a blog using : GET /api/blog/:id
@@ -14,13 +14,13 @@ router.get("/:id", getSingleBlog)
 router.get("/", getBlogs)
 
 // get all blogs of a author using : GET /api/blog/author/:id
-// router.get("/author/:id", fetchUser, getBlogs)
+router.get("/author/:id", getAuthorBlogs)
 
-// update a blog using : PATCH /api/blog/:id  : token require
-// router.patch("/:id", fetchUser, updateBlog)
+// update a blog using : PUT /api/blog/:id  => token require
+router.put("/:id", fetchUser, updateBlog)
 
-// delete a blog using : DELETE /api/blog/:id  : token require
-// router.delete("/:id", fetchUser, deleteBlog)
+// delete a blog using : DELETE /api/blog/:id  => token require
+router.delete("/:id", fetchUser, deleteBlog)
 
 
 module.exports = router
