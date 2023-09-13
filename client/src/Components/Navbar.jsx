ab28c6d0f1e7217fdc78098a57afeb65c7aefc2e
Arghya Maity <maityarghya0@gmail.com>
2023-09-13 23:50:19 +0530

    add default profile photo for unauthorize users

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -5,52 +5,61 @@ import UserActionsOverlay from "./UserActionsOverlay";
 import { toggleSlide } from "../features/info/infoSlice";
 
 function Navbar() {
-  const [userData, setUserData] = useState(null)
+  const [userData, setUserData] = useState(null);
   const dispatch = useDispatch();
 
   const isSlideOpen = useSelector((state) => state.info.isSlideOpen);
   const token = useSelector((state) => state.auth.token);
   const id = useSelector((state) => state.auth.id);
-  const serverBaseUrl = import.meta.env.VITE_Server_BASE_URL;  
+  const serverBaseUrl = import.meta.env.VITE_Server_BASE_URL;
 
   const handleSliderState = () => {
     console.log("clicked");
-    dispatch(toggleSlide())
-  }
+    dispatch(toggleSlide());
+  };
 
   useEffect(() => {
-    const fetchAuthor = async () => {
-      try {
-        const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
-        const authorApi = `${apiBaseUrl}/api/author/${id}`;
-        const res = await fetch(authorApi);
-        const authorData = await res.json();
-        setUserData(authorData)
-      } catch (error) {
-        console.log(error);
-      }
-    };
+    if (token) {
+      const fetchAuthor = async () => {
+        try {
+          const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+          const authorApi = `${apiBaseUrl}/api/author/${id}`;
+          const res = await fetch(authorApi);
+          const authorData = await res.json();
+          setUserData(authorData);
+        } catch (error) {
+          console.log(error);
+        }
+      };
 
-    fetchAuthor()
-  }, [])
-  
-  return (<>
-    <div className="px-[calc((100vw-1280px)/2)] border-b-2 flexCenter ">
-      <h1 className="text-zinc-900 font-KenyanCoffee text-4xl m-4">
-        Insight Social
-      </h1>
-      <div className="mx-4 flexCenter space-x-3">
-        {token ? (
+      fetchAuthor();
+    }
+  }, []);
+
+  return (
+    <>
+      <div className="px-[calc((100vw-1280px)/2)] border-b-2 flexCenter ">
+        <h1 className="text-zinc-900 font-KenyanCoffee text-4xl m-4">
+          Insight Social
+        </h1>
+        <div className="mx-4 flexCenter space-x-3">
           <img
             className="w-10 h-10 rounded-full object-cover cursor-pointer"
-            src={`${serverBaseUrl}/assets/avatar/${userData?.avatarImgName}`}
+            src={
+              token
+                ? `${serverBaseUrl}/assets/avatar/${userData?.avatarImgName}`
+                : "public/assets/profileIconDefault.png"
+            }
             alt="Avatar"
             onClick={handleSliderState}
           />
-        ) : undefined}
+        </div>
       </div>
-    </div>
-    <UserActionsOverlay isSlideOpen={isSlideOpen} handleSliderState={handleSliderState} userData={userData} />
+      <UserActionsOverlay
+        isSlideOpen={isSlideOpen}
+        handleSliderState={handleSliderState}
+        userData={userData}
+      />
     </>
   );
 }
